46d9466201bdc8fc1f29e8c8d42d4ec8a5d29544
velimir-kovacic <velimir.kovacic.zg@gmail.com>
2022-10-26 18:56:24 +0200

    dodan generator

--- LAB1/generator.py
+++ LAB1/generator.py
@@ -1,4 +1,13 @@
-file_object = open('analizator/analizator.py', 'a')
-file_object.write("print(\"hello\")")
+import ulaz
+import json
 
-file_object.close()
+lekser = open('analizator/analizator.py', 'a')
+
+poc_stanje, stanja, akcije = ulaz.ulaz()
+
+lekser.write("pocetno_stanje = {}\n".format(json.dumps(poc_stanje)))
+lekser.write("stanja = {}\n".format(json.dumps(stanja)))
+for akcija in akcije:
+    lekser.write("akcije += [Akcija({})]\n".format(akcija))
+
+lekser.close()
