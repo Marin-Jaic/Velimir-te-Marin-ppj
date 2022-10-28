8361f99f464567d5a72c6dac82412c602fa70a4f
Marin Jaic <mjaic3@gmail.com>
2022-10-28 14:44:44 +0200

    Konceptualno osposbljen analizator

--- LAB1/ppj_lab1_v2/main.py
+++ LAB1/ppj_lab1_v2/main.py
@@ -13,15 +13,19 @@ updated_akcije_tracker = []
 current_max = 0 #vjerojatno bespotreban
 pocetni_index = 0
 finalna_akcija = None
+redak_counter = 0
 
 for redak in retci:
+    redak_counter += 1
     novi_redak = False
     i = 0
     while(i < len(redak)):
 
         if(novi_izraz): #PRIPREMA ZA OBRADU NOVOG NIZA
+            akcije_tracker = [] #resettamo tracker
             current_max = 0
             pocetni_index = i
+            finalna_akcija = None
 
             for akcija in akcije:
                 if akcija.stanje == trenutno_stanje:
@@ -34,6 +38,7 @@ for redak in retci:
         index = 0
         max_updated = False
         
+
         for tracker in akcije_tracker:  #Parsiranje niza kroz sve moguće automate
 
             if tracker[0].automat.prijelaz(redak[i]):   #postoji li prijelaz za akciju s trenutnim znakom
@@ -47,11 +52,11 @@ for redak in retci:
                 # elif max_updated and current_max == tracker[1]:
                 #     updated_akcije_tracker.append(tracker)
 
-            elif tracker[0].automat.izraz_prihvacen and current_max < tracker[1]: #ne postoji, provjeravamo je li niz prihvacen na zadnjem znako i je li njegova duljina duza od najvece zabiljezene
-                finalna_akcija = tracker[0]
+            elif tracker[0].automat.izraz_prihvacen and current_max < tracker[1] or tracker[0].automat.izraz_prihvacen and current_max == tracker[1] and int(finalna_akcija.id) > int(tracker[0].id): 
+                finalna_akcija = tracker[0] #ne postoji, provjeravamo je li niz prihvacen na zadnjem znaku i je li njegova duljina duza od najvece zabiljezene
                 current_max = tracker[1]
             
-        if not len(updated_akcije_tracker): #Nema vise mogucih prijelaza, nece biti daljnje finalne akcije
+        if not len(updated_akcije_tracker) and finalna_akcija is not None: #Nema vise mogucih prijelaza, nece biti daljnje finalne akcije
             if int(finalna_akcija.vrati_se):
                 i = pocetni_index + int(finalna_akcija.vrati_se)
             
@@ -61,11 +66,17 @@ for redak in retci:
             if finalna_akcija.lex != "":
                 gas = 0
                 #OBRADA LEKSICKE JEDINKE I ZAPIS MOZDA IDFK MORAM JOS RAZRADIT
+                print(finalna_akcija.lex + " " + str(redak_counter) + " " + redak[pocetni_index:i])
 
             if finalna_akcija.novi_redak:
                 novi_redak = True
             
-            akcije_tracker = [] #resettamo tracker
+            novi_izraz = True
+
+        elif not len(updated_akcije_tracker) and current_max != i - pocetni_index:
+            #nema prijelaza i ne postoji definirana finalna akcija -> greske
+            print(redak[pocetni_index], file = sys.stderr)
+            i = pocetni_index + 1
             novi_izraz = True
 
         else:   #ima prijelaza u akcijama spremljenima u updated_ackije_tracker, prenosimo ih u akcije_tracker
